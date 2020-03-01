747e3db9c13de7b99102b236da675d04647653e6
madking55 <madking55@gmail.com>
2020-03-01 11:28:42 +0100

    Add expired? method

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -10,42 +10,35 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      
-      # Brie 
-      if item.name == AGED_BRIE
-        increase_quality(item, 1)
 
-      # Backstage
-      elsif item.name == BACKSTAGE_PASS
+      if item.name != SULFURUS
+        item.sell_in = item.sell_in - 1
+      end
+
+      case item.name
+      when AGED_BRIE
         increase_quality(item, 1)
-            if item.sell_in < 11
+      when BACKSTAGE_PASS
+        increase_quality(item, 1)
+            if item.sell_in < 10
               increase_quality(item, 1)
             end
-            if item.sell_in < 6
+            if item.sell_in < 5
               increase_quality(item, 1)
             end
-
-      # Sulfurus and default 
+      when SULFURUS
+        #  do nothing
       else 
         decrease_quality(item, 1)
       end
 
-
-
-      #  Sulfurus
-      if item.name != SULFURUS
-        item.sell_in = item.sell_in - 1
-      end
-      
       #  default item
-      if item.sell_in < 0
-
+      if expired?(item)
         if item.name == AGED_BRIE
           increase_quality(item, 1)
         elsif item.name == BACKSTAGE_PASS
           decrease_quality(item, item.quality) # drops quality to 0 after concert
         elsif item.name == SULFURUS
-
         else
           decrease_quality(item, 1)
         end
@@ -53,16 +46,16 @@ class GildedRose
     end
   end
 
+  def expired?(item)
+    item.sell_in < 0
+  end
+
   def increase_quality(item, quality_raise)
-    if item.quality < 50
-      item.quality += quality_raise
-    end
+    item.quality += quality_raise if item.quality < 50
   end
 
   def decrease_quality(item, quality_drop)
-    if item.quality > 0
-      item.quality -= quality_drop
-    end
+    item.quality -= quality_drop if item.quality > 0
   end
 
   
